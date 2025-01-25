676ff25544617b757edabf9eb3ecc134bd80ad7e
jasnli <167535698+jasnli@users.noreply.github.com>
2025-01-25 01:13:43 +0000

    dwuiwe

--- src/main/java/com/example/project/BookStore.java
+++ src/main/java/com/example/project/BookStore.java
@@ -17,9 +17,11 @@ public class BookStore{
      */
     public void addUser(User user){
         boolean isFilled = false;
+        // checks every index and if the index is empty and we haven't already filled an index yet, add that user to the index
         for (int i = 0; i < users.length; i++) {
             if (users[i] == null && !isFilled) {
                 users[i] = user;
+                // isFilled to true to prevent that user from being added to every index
                 isFilled = true;
             }
         }
@@ -31,10 +33,12 @@ public class BookStore{
      */
     public void removeUser(User user){
         for (int i = 0; i < users.length; i++) {
+            // if that user in the bookstore's matches the id of the user we want to remove, replace it with null
             if (users[i] != null && users[i].getId().equals(user.getId())) {
                 users[i] = null;
             }
         }
+        // shifts the list over
         consolidateUsers();
     }
     /**
@@ -42,6 +46,9 @@ public class BookStore{
      */
     public void consolidateUsers(){
         for (int i = 0; i < users.length; i++) {
+            // if there is a null spot, search the rest of the list for an index that isn't null and pull that user to the current null spot
+            // then replace that users old spot with a null, this way only the first instances of nulls will be replaced and all the
+            // nulls will be on the right in the end
             if (users[i] == null) {
                 for (int j = i; j < users.length; j++) {
                     if (users[j] != null) {
@@ -59,11 +66,15 @@ public class BookStore{
      * @param book
      */
     public void addBook(Book book){
+        // makes a new list with the length but one more spot for the new book
         Book[] newBooks = new Book[books.length + 1];
+        // copies the list without making it a reference to the memory address
         for (int i = 0; i < books.length; i++) {
             newBooks[i] = books[i];
         }
+        // adds that book to the very end
         newBooks[books.length] = book;
+        // assigns the copy to the original
         books = newBooks;
     }
     // inserts a book at a certain index in the books list
@@ -75,41 +86,62 @@ public class BookStore{
     public void insertBook(Book book, int index){
         // if the index has a book already
         Book[] newBook = new Book[books.length + 1];
+        // make a copy with one more spot for the new book
         for (int i = 0; i < index; i++) {
+            // copies until the index we want, keeping the same as the original
             newBook[i] = books[i];
         }
+        // puts the book at that index
         newBook[index] = book;
+        // shifts the rest of the books over by 1 spot
         for (int i = index + 1; i < newBook.length; i++) {
             newBook[i] = books[i - 1];
         }
+        // sets the original to the copy to avoid memory address referencing
         books = newBook;
     }
+
     // removes the a certain book from the books list
+    /**
+     * 
+     * @param book the book to be removed
+     */
     public void removeBook(Book book){ 
+        // iterate through the book list
         for (int i = 0; i < books.length; i++) {
+            // check if the isbn is equal
             if (books[i].getIsbn().equals(book.getIsbn())) {
+                // takes the quantity and takes one away
                 books[i].setQuantity(books[i].getQuantity() - 1);
                 if (books[i].getQuantity() == 0){ 
+                    // creates a copy with one less for the shortened list
                     Book[] newBooks = new Book[books.length - 1];
+                    // if there is no more of that book, remove it entirely from the list
                     int indexNew = 0;
                     for (int j = 0; j < books.length; j++) {
                         if (books[j].getQuantity() != 0) {
                             newBooks[indexNew] = books[j];
+                            // adds an index so we can shift it over by 1 when we hit that book
                             indexNew ++;
                         }
                     }
+                    // assigns it back
                     books = newBooks;
                 }
             }
         }
     }
 
-       
+       /**
+        * 
+        * @return a string of the bookstore's book info
+        */
     public String bookStoreBookInfo(){
         String str = "";
         for (int i = 0; i < books.length ; i ++) {
+            str += i + 1 + ". ";
             if (books[i] == null) {
-                str += "empty \n";
+                str += "Empty \n";
             } else {
                 str += books[i].bookInfo() + "\n";  
             }
@@ -118,11 +150,15 @@ public class BookStore{
         return str;
     } //you are not tested on this method but use it for debugging purposes
 
+    /**
+     * 
+     * @return a string of the user and their infos
+     */
     public String bookStoreUserInfo(){
         String str = "";
         for (int i = 0; i < users.length; i ++) {
             if (users[i] == null) {
-                str += "null \n";
+                str += "EMPTY SPOT \n";
             } else {
                 str += users[i].userInfo() + "\n";
             }
--- src/main/java/com/example/project/Main.java
+++ src/main/java/com/example/project/Main.java
@@ -1,21 +1,268 @@
 package com.example.project;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        BookStore store = new BookStore(); 
-        Book b1 = new Book("The Great Gatsby","Scott Fitzgerald", 1925, "979-8351145013",3);
-        Book b2 = new Book("The Outliers", "Malcolm Gladwell",2008,"978-0316017930",1);
-        Book b3 = new Book("1984", "George Orwell", 1949, "978-0451524935", 5);
-        Book b4 = new Book("Brave New World", "Aldous Huxley", 1932, "978-0060850524", 3);
-        Book b5 = new Book("Test","Author",1900, "1234", 1);
-        store.addBook(b1);store.addBook(b4);
-        System.out.println(store.bookStoreBookInfo());
-        store.insertBook(b2, 1);
-        System.out.println(store.bookStoreBookInfo());
-        store.insertBook(b3,2);
-        System.out.println(store.bookStoreBookInfo());
-        store.insertBook(b5,4);
-        System.out.println(store.bookStoreBookInfo());
+    private static int printLayout() {
+        Scanner s = new Scanner(System.in);
+        System.out.println("𖦹------------Select One of the Following-------------𖦹");
+        System.out.println("0️⃣  Exit Application ☹️");
+        System.out.println("1️⃣  Add a New Book to the Jamazon Library 📖");
+        System.out.println("2️⃣  Upgrade Quantity of a Book 📈");
+        System.out.println("3️⃣  Search for a Book 🔍");
+        System.out.println("4️⃣  Show All Books 👁️");
+        System.out.println("5️⃣  Register A Student 👨‍🎓");
+        System.out.println("6️⃣  Remove/Show Registered Students 📃");
+        System.out.println("7️⃣  Check Out Book 📩");
+        System.out.println("8️⃣  Remove Book 🛫");
+        System.out.println("𖦹----------------------------------------------------𖦹");
+        System.out.println("Choose An Option: ");
+        return s.nextInt();
+    }
 
+    public static void main(String[] args) {
+        BookStore jamazon = new BookStore();
+        Scanner s = new Scanner(System.in);
+        System.out.println("------⭐ - • Welcome to Jamazon Library • - ⭐------");
+        System.out.println("                       Welcome!");
+        System.out.println("--------------------= - • ✅ • - =--------------------");
+        int choice = Integer.MIN_VALUE;
+        boolean done = true;
+        while(choice != 0) {
+            // PRINT LAYOUT IF ACTION IS FINISHED
+            if (done) {
+                choice = printLayout();
+                done = false;
+            }
+            // REPEAT ASKING IF THE CHOICES ARE NOT A CHOICE AVAILABLE
+            if (choice > 8 || choice < 0) {
+                done = true;
+            }
+            // ADD A NEW BOOK TO THE LIBRARY
+            if (choice == 1) {
+                String sn;
+                String bookName;
+                String author;
+                int yearP;
+                int quant;
+                System.out.println("▮▮▮ |] ACTION: ADDING BOOK ---------------- [|");
+                // ASKING FOR ALL THE BOOK INFO TO ADD
+                System.out.print("Enter the Serial Number of the Book: ");
+                sn = s.nextLine();
+                System.out.print("Enter the Book Title: ");
+                bookName = s.nextLine();
+                System.out.print("Enter the Book Author: ");
+                author = s.nextLine();
+                System.out.print("Enter the Year the Book was Published: ");
+                yearP = s.nextInt();
+                s.nextLine();
+                System.out.print("Quantity of Books Added: ");
+                quant = s.nextInt();
+                s.nextLine();
+                // MAKING A NEW BOOK OBJECT TO ADD TO THE LIST'S END
+                jamazon.addBook(new Book(bookName, author, yearP, sn, quant));
+                System.out.println("✅ BOOK ADDED ✅");
+                // ACTION FINISHED
+                done = true;
+            }
+            // UPGRADING THE QUANTITY OF A BOOK 
+            if (choice == 2) {
+                System.out.println("▮▮▮ |] ACTION: UPGRADING QUANTITY ---------------- [|");
+                System.out.println(jamazon.bookStoreBookInfo());
+                System.out.println("• Select A Book (NUMBER) •");
+                // FOR THE INDEX OF THE LIST IN THE BOOKSTORE
+                int selection = s.nextInt() - 1;
+                s.nextLine();
+                if (selection < jamazon.getBooks().length) {
+                    // TO SELECT NEW QUANTITY
+                    System.out.print("New Quantity for " + jamazon.getBooks()[selection].getTitle() + ": ");
+                    jamazon.getBooks()[selection].setQuantity(s.nextInt());
+                    s.nextLine();
+                    System.out.println("! QUANTITY UPDATED !");
+                    // IF CHOICE IS INVALID (SUCH AS CHOOSING A BOOK THAT DOESN'T EXIST)
+                } while (!(selection < jamazon.getBooks().length)) {
+                    System.out.print("| - ⚠️ Invalid Entry, Try Again ⚠️ - |");
+                    selection = s.nextInt() - 1;
+                }
+                // ACTION DONE
+                done = true;
+            }  
+            // SEARCH FOR A BOOK
+            if (choice == 3){
+                System.out.println("▮▮▮ |] ACTION: SEARCH FOR A BOOK ---------------- [|");
+                System.out.println("Choose a Search Method");
+                // TWO METHODS, TITLE AND SERIAL NUMBER
+                System.out.println("1. Title");
+                System.out.println("2. Serial Number");
+                // SELECTS THE METHOD BY NUMBER 1 = TITLE , 2 = SN
+                int method = s.nextInt();
+                s.nextLine();
+                // REPEATS IF THE ENTRY IS NOT 1 OR 2
+                while (method <= 0 || method > 2) {
+                    System.out.println("⚠️ Invalid Option, Try Again ⚠️");
+                    method = s.nextInt();
+                    s.nextLine();
+                }
+                // BY TITLE SEARCH
+                if (method == 1) {
+                    System.out.print("Enter Book Title: ");
+                    String enteredTitle = s.nextLine();
+                    boolean foundBook = false;
+                    // CHECKS THROUGH THE LIST FOR A BOOK WITH THE SAME TITLE AND THEN ENDS THE LOOP WHEN IT FINDS IT WITH THE FOUNDBOOK BOOLEAN VARIABLE
+                    for (int i = 0; i < jamazon.getBooks().length; i++) {
+                        if (jamazon.getBooks()[i].getTitle().equals(enteredTitle)) {
+                            System.out.println(jamazon.getBooks()[i].bookInfo());
+                            foundBook = true;
+                        }
+                        // IF IT DOESNT FIND THE BOOK, REPORT BOOK NOT FOUND
+                    } if (!foundBook) {
+                        System.out.println("⚠️ BOOK NOT FOUND ⚠️");
+                    }
+                    // BY THE SERIAL NUMBER
+                } if (method == 2) {
+                    System.out.print("Enter Book Serial Number: ");
+                    String enteredSN = s.nextLine();
+                    boolean foundBook = false;
+                    // SAME AS ABOVE, BUT FOR SERIAL NUMBER INSTEAD
+                    for (int i = 0; i < jamazon.getBooks().length; i++) {
+                        if (jamazon.getBooks()[i].getIsbn().equals(enteredSN)) {
+                            System.out.println(jamazon.getBooks()[i].bookInfo());
+                            foundBook = true;
+                        }
+                    } if (!foundBook) {
+                        System.out.println("⚠️ BOOK NOT FOUND ⚠️");
+                    }
+                }
+                // ACTION COMPLETED
+                done = true;
+            }
+            // SHOW ALL BOOKS
+            if (choice == 4) {
+                System.out.println("▮▮▮ |] ACTION: SHOW ALL BOOKS ---------------- [|");
+                System.out.println(jamazon.bookStoreBookInfo());
+                // FOR INSERTING A BOOK, Y FOR YES, N FOR NO
+                System.out.println("Would you like to insert a book? (y/n)");
+                String option = s.nextLine();
+                // IF IT IS YES (Y)
+                if (option.toLowerCase().equals("y")) {
+                    // PRECONDITION: POSITION MUST BE IN THE LIST ALREADY (CANNOT BE GREATER THAN LIST LENGTH)
+                    System.out.println("- Enter Position to Insert -");
+                    int position = s.nextInt() - 1;
+                    s.nextLine();
+                    System.out.println("- Enter Book Info to Insert -");
+                    System.out.print("Title: ");
+                    String insTitle = s.nextLine();
+                    System.out.print("Author: ");
+                    String insAuthor = s.nextLine();
+                    System.out.print("Serial Number: ");
+                    String insBN = s.nextLine();
+                    System.out.print("Year Published: ");
+                    int insYP = s.nextInt();
+                    s.nextLine();
+                    System.out.print("Quantity: ");
+                    int insQU = s.nextInt();
+                    s.nextLine();
+                    // INSERTS THE BOOK WITH THE GIVEN DATA
+                    jamazon.insertBook(new Book(insTitle, insAuthor, insYP, insBN, insQU), position);
+                }
+                // ACTION DONE
+                done = true;
+            }
+            // REGISTER A STUDENT
+            if (choice == 5) {
+                System.out.println("▮▮▮ |] ACTION: REGISTERING A STUDENT ---------------- [|");
+                System.out.println("- Add A New Student -");
+                System.out.print("👨‍🎓 ");
+                // ADDS A NEW USER AS A STUDENT
+                jamazon.addUser(new User(s.nextLine(), IdGenerate.generateID()));
+                // ACTION COMPLETE
+                done = true;
+            }
+            // SHOW/REMOVE REGISTERED STUDENTS
+            if (choice == 6) {
+                // SHOWS THE STUDENTS FIRST
+                System.out.println("▮▮▮ |] ACTION: SHOW OR EDIT REGISTERED STUDENTS ---------------- [|");
+                System.out.println(jamazon.bookStoreUserInfo());
+                // ASK FOR REMOVE (Y FOR YES, ANYTHING ELSE FOR NO)
+                System.out.print("Would you like to remove a student? ");
+                String selection = s.nextLine();
+                // IF WE WANNA REMOVE
+                if (selection.toLowerCase().equals("y")) {
+                    System.out.println("Select ID of Student To Remove");
+                    // SELECT THE STUDENT'S ID
+                    String studentID = s.nextLine();
+                    for (int i = 0; i < jamazon.getUsers().length; i++) {
+                        // IF THE ID IS A STUDENT'S ID FROM THE STORE, REMOVE THAT STUDENT
+                        if (studentID.equals(jamazon.getUsers()[i].getId())) {
+                            User student = jamazon.getUsers()[i];
+                            jamazon.removeUser(student);
+                            i = jamazon.getUsers().length + 1;
+                        }
+                    } 
+                }
+
+                // ACTION COMPLETE
+                done = true;
+            }
+            // CHECK OUT BOOK
+            if (choice == 7) {
+                System.out.println("▮▮▮ |] ACTION: CHECK OUT A BOOK FOR A STUDENT ---------------- [|");
+                System.out.println(jamazon.bookStoreUserInfo());
+                System.out.println("Select ID of Student");
+                // PRECONDITION: MUST BE VALID ID
+                String studentId = s.nextLine();
+                System.out.println(jamazon.bookStoreBookInfo());
+                System.out.println("Select Book to Check Out (List Number)");
+                // PRECONDITION: MUST BE VALID NUMBER FROM THE LIST
+                int bookFromList = s.nextInt() - 1;
+                s.nextLine();
+                // CHECKS THROUGH EVERY USER
+                for (int i = 0; i < jamazon.getUsers().length; i++) {
+                    boolean end = false;
+                    // IF THE IDS MATCH
+                    if (studentId.equals(jamazon.getUsers()[i].getId())) {
+                        // MAKE A COPY OF THE BOOK LIST THAT USER HAS THAT HAS THE MATCHING ID
+                        Book[] userBookList = new Book[5];
+                        for (int k = 0; k < 5; k++) {
+                            userBookList[k] = jamazon.getUsers()[i].getBooks()[k];
+                        }
+                        // CHECKS THROUGH EVERY INDEX IN THAT BOOKLIST
+                        // IF ONE OF THEM IS EMPTY, AND THE ACTION HASNT ENDED YET
+                        // PUT A NEW BOOK THERE
+                        for (int j = 0; j < 5; j++) {
+                            if (userBookList[j] == null && !end) {
+                                Book book = jamazon.getBooks()[bookFromList];
+                                userBookList[j] = new Book(book.getTitle(), book.getAuthor(), book.getYearPublished(), book.getIsbn(), 1);
+                                end = true;
+                                // IF THE BOOK IS ALREADY THERE JUST ADD TO THE QUANTITY
+                            } else if (!end && userBookList[j].getIsbn().equals(jamazon.getBooks()[bookFromList].getIsbn())) {
+                                userBookList[j].setQuantity(userBookList[j].getQuantity() + 1);
+                                end = true;
+                            }
+                            // END THE ACTION AFTERWARDS
+                        }
+
+                        // REMOVES THE BOOK FROM JAMAZON
+                        jamazon.removeBook(jamazon.getBooks()[bookFromList]);
+                        jamazon.getUsers()[i].setBooks(userBookList);
+                        // ENDS LOOP
+                        i = jamazon.getUsers().length + 1;
+                    }
+                }
+                // ACTION COMPLETE
+                done = true;
+            }
+            // REMOVE BOOK
+            if (choice == 8) {
+                System.out.println("▮▮▮ |] ACTION: CHECK OUT A BOOK FOR A STUDENT ---------------- [|");
+                System.out.println(jamazon.bookStoreBookInfo());
+                // SELECT A BOOK THEN REMOVE IT
+                System.out.println("List Number of Book to Remove");
+                int indexNum = s.nextInt() - 1;
+                s.nextLine();
+                jamazon.removeBook(jamazon.getBooks()[indexNum]);
+                // ACTION COMPLETE
+                done = true;
+            }
+        }
     }
 }
